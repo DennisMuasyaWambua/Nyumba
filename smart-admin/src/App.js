6d2b5e4743c50791816d46b34f5a11b588a19ad2
Peter Ochieng <peterochieng94@gmail.com>
2023-01-17 22:17:20 +0300

    Login Page Skeleton
    Login Page Skeleton

---
+++ smart-admin/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LandingNavigation from './components/LandingNavigation';
+
+function App() {
+  return (
+    <div className="App">
+     <LandingNavigation></LandingNavigation>
+    </div>
+  );
+}
+
+export default App;
